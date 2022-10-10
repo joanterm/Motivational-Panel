6830961de9765fb2eaf626284ad245a3d3b87588
joanterm <jo_joanarg@yahoo.com>
2022-10-10 15:11:34 -0400

    Initial form markup on client side set up

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import {useEffect, useState} from "react"
 
 function App() {
   const [backend, setBackend] = useState([])
+  const [formData, setFormData] = useState({
+    quoteText: "",
+    authorText: ""
+  })
 
   useEffect(() => {
     fetch("/api")
@@ -16,6 +20,18 @@ function App() {
     })
   }, [])
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <div>
       <h1>Test react</h1>
@@ -25,6 +41,23 @@ function App() {
         <h2>{e.author}</h2>
       </div>
       )}
+    <form onSubmit={handleSubmit}>
+        <label htmlFor="quoteText">Quote:</label>
+        <input 
+          type="text"
+          name="quoteText"
+          value={formData.quoteText}
+          onChange={handleChange}
+        />
+        <label htmlFor="authorText">Author:</label>
+        <input 
+          type="text"
+          name="authorText"
+          value={formData.authorText}
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+    </form>
     </div>
   );
 }
