05e422cdcbae0122baaf53991eebdff68f5cf833
joanterm <jo_joanarg@yahoo.com>
2023-04-11 13:33:00 -0400

    Completed user registration on front end

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,7 +1,58 @@
+import {useEffect, useState} from "react"
+import axios from "axios"
+
 const SignUp = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        password: ""
+    })
+
+    const handleSignupSubmit = (e) => {
+        e.preventDefault()
+        axios
+        .post("/auth", {
+            username: signUpData.username,
+            password: signUpData.password
+        })
+        .then((response) => {
+            console.log(response.data)
+            console.log("successful registration")
+        })
+        .catch((err) => {
+            console.log("Error")
+        })          
+    }
+
+    const handleSignupChange = (e) => {
+        setSignUpData({
+            ...signUpData,
+            [e.target.name]: e.target.value
+        })
+        console.log(signUpData);    
+    }
+
+
     return ( 
         <div>
             <h1>SIGN UP</h1>
+        <form onSubmit={handleSignupSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input 
+              type="text"
+              name="username"
+              value={signUpData.username}
+              onChange={handleSignupChange}
+            />
+            <label htmlFor="password">Password:</label>
+            <input 
+              type="text"
+              name="password"
+              value={signUpData.password}
+              onChange={handleSignupChange}
+            />
+            <button className="submit-button">Submit</button>
+        </form>
+
         </div>
      );
 }
